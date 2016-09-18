44978d6dced40774373185393cd6b05a0a333ac0
Bip Thelin <bip@kivra.com>
2016-09-18 20:12:53 +0200

    Map media keys

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,6 +4,8 @@
 
 --_* Imports ==========================================================
 import           XMonad
+import           XMonad.Actions.Volume
+import           XMonad.Config.Gnome      (gnomeConfig)
 import           XMonad.Hooks.DynamicLog
 import           XMonad.Hooks.ManageDocks
 import           XMonad.Hooks.SetWMName
@@ -12,24 +14,38 @@ import           XMonad.Util.Run          (spawnPipe)
 
 import           System.IO
 
+--_* Types ============================================================
+myNormalBorderColor  = "#000000"
+myFocusedBorderColor = "#93c91d"
+
 --_* Code =============================================================
 --_ * API -------------------------------------------------------------
 main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ defaultConfig
-    { manageHook      = manageDocks <+> manageHook defaultConfig
-    , layoutHook      = avoidStruts  $  layoutHook defaultConfig
-    , startupHook     = setWMName "LG3D"
-    , handleEventHook = mconcat
-    [ docksEventHook
-    , handleEventHook defaultConfig ]
-    , logHook     = dynamicLogWithPP xmobarPP
+    { manageHook         = manageDocks <+> manageHook defaultConfig
+    , layoutHook         = avoidStruts  $  layoutHook defaultConfig
+    , startupHook        = setWMName "LG3D"
+    , modMask            = mod4Mask     -- Rebind Mod to the Cmd key
+    , normalBorderColor  = myNormalBorderColor
+    , focusedBorderColor = myFocusedBorderColor
+    , borderWidth        = 2
+    , handleEventHook    = mconcat
+      [ docksEventHook
+      , handleEventHook defaultConfig ]
+    , logHook            = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
       , ppTitle  = xmobarColor "green" "" . shorten 50
       }
-    , modMask = mod4Mask     -- Rebind Mod to the Cmd key
     } `additionalKeys`
     [ ((mod4Mask .|. shiftMask, xK_z), spawn "slock")
+    , ((0, 0x1008ff02), spawn "xcalib -c")
+    , ((0, 0x1008ff03), spawn "xcalib -co 90 -a")
+    , ((0, 0x1008ff05), spawn "kbdlight up")
+    , ((0, 0x1008ff06), spawn "kbdlight down")
+    , ((0, 0x1008ff12), toggleMuteChannels ["Master", "Headphone", "Speaker", "Bass Speaker"] >> return ())
+    , ((0, 0x1008ff13), raiseVolume 4 >> return ())
+    , ((0, 0x1008ff11), lowerVolume 4 >> return ())
     ]
 
 --_* Editor ===========================================================
